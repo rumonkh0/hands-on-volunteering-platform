736b7af9070a0794fc495a3c348c9c5f0852181c
rumonkh0 <rummankh0@gmail.com>
2025-03-09 21:32:04 +0600

    feat: connect backend to MongoDB database

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Enable CORS
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Welcome to hand on volunteering!" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+});
